3a0573940fb9048663af3442766b82a5051b9352
Wandile <wandilenkovu22@gmail.com>
2023-11-30 10:58:37 -0800

    my first toolbox

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Addition function
+
+    Args:
+    a: first integer
+    b: first integer
+
+    Returns:
+    the return value. a + b
+
+"""
+    return (a + b)
+
+def sub(a, b):
+    """Subtraction function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    the return value. a - b
+"""
+    return (a -b)
+
+def mul(a, b):
+    """multiplication function
+
+    Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    the return value. a * b
+"""
+    return (a * b)
+
+def div(a, b):
+    """division function
+
+Args:
+    a: first integer
+    b: second integer
+
+    Returns:
+    the return value. a / b
+"""
+    return int(a / b)
